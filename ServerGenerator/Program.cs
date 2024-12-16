99f2ced6f52c9c05996d79f6460956706c74e077
shlmt <sh7618sh@gmail.com>
2024-12-16 17:01:11 +0200

    server of minimalApi .net with mysql

--- ServerGenerator/Program.cs
+++ ServerGenerator/Program.cs
@@ -1,75 +1,101 @@
 ﻿using ServerGenerator;
+using ServerGenerator.minimal;
 using ServerGenerator.nodejs;
 
-Model[] models = [
-   new NodeModel()
-    {
-        Name = "person",
-        Properties = [
-            new NodeProperty()
-            {
-                Name = "name",
-                Type = DataType.String,
-                DefaultValue = "123",
-                IsRequired = true,
-                IsUnique = true,
-                IsLowerCase = true,
-                IsTrim = true
-            },
-            new NodeProperty()
-            {
-                Name = "age",
-                Type = DataType.Number,
-                Min = 0,
-                Max = 120
-            },
-            new NodeProperty()
-            {
-                Name = "numbers",
-                Type = DataType.Doubles,
-                DefaultValue = "[1.5,2,3]"
-            }
-        ]
-    },
-    new NodeModel()
-    {
-        Name = "item",
-        Properties = [
-            new NodeProperty()
-            {
-                Name = "type",
-                Type = DataType.String,
-                Enum = ["a","b","c"],
-                IsImmutable = true,
-            },
-            new NodeProperty()
-            {
-                Name = "desc",
-                Type = DataType.String,
-                Min = 0,
-                Max = 50
-            },
-            new NodeProperty()
-            {
-                Name = "creator",
-                Type = DataType.ObjectId,
-                Ref = "person"
-            }
-        ]
-    }
-];
+//Model[] models = [
+//   new NodeModel()
+//    {
+//        Name = "person",
+//        Properties = [
+//            new NodeProperty()
+//            {
+//                Name = "name",
+//                Type = DataType.String,
+//                DefaultValue = "123",
+//                IsRequired = true,
+//                IsUnique = true,
+//                IsLowerCase = true,
+//                IsTrim = true
+//            },
+//            new NodeProperty()
+//            {
+//                Name = "age",
+//                Type = DataType.Number,
+//                Min = 0,
+//                Max = 120
+//            },
+//            new NodeProperty()
+//            {
+//                Name = "numbers",
+//                Type = DataType.Doubles,
+//                DefaultValue = "[1.5,2,3]"
+//            }
+//        ]
+//    },
+//    new NodeModel()
+//    {
+//        Name = "item",
+//        Properties = [
+//            new NodeProperty()
+//            {
+//                Name = "type",
+//                Type = DataType.String,
+//                Enum = ["a","b","c"],
+//                IsImmutable = true,
+//            },
+//            new NodeProperty()
+//            {
+//                Name = "desc",
+//                Type = DataType.String,
+//                Min = 0,
+//                Max = 50
+//            },
+//            new NodeProperty()
+//            {
+//                Name = "creator",
+//                Type = DataType.ObjectId,
+//                Ref = "person"
+//            }
+//        ]
+//    }
+//];
 
 ProjectInfo.RootDirectory = "C:\\Users\\משתמש\\הנדסת תוכנה";
-ProjectInfo.ProjectName = "example-server";
+ProjectInfo.ProjectName = "exampleApi";
 
-Server server = new NodeServer()
+//Server server = new NodeServer()
+//{
+//    Language = "nodejs",
+//    Database = "mongodb",
+//    DataBaseURL = "mongodb://localhost:27017/example",
+//    Models = models
+//};
+
+Model[] models2 = [new NodeModel()
 {
-    Language = "nodejs",
-    Database = "mongodb",
-    DataBaseURL = "mongodb://localhost:27017/example",
-    Models = models
-};
+    Name = "item",
+    Properties = [new NodeProperty() {
+        Name = "Id",
+        Type = DataType.String,
+    },
+    new NodeProperty(){
+        Name = "Name",
+        Type = DataType.String,
+    },
+    new NodeProperty(){
+        Name = "IsCompleted",
+        Type = DataType.Boolean,
+    }
+    ]
+}];
 
+Server server = new MinNetServer()
+{
+    Language = "c#",
+    Database = "mysql",
+    DataBaseURL = "server=localhost;user=root;password=1234;database=todos",
+    Models = models2
+};
 bool success = server.Generate();
 
 Console.WriteLine(ProjectInfo.ProjectName+" "+ success);
